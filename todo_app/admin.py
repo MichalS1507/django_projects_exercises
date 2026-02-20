13f5d48967ee214017edd9eab5390ec392d95765
MichalS1507 <michal.smatlak@gmail.com>
2026-02-20 12:46:31 +0100

    Made improvements in models and admin of todo app

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Category,Task
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color']
+    list_filter = ['name']
+    search_fields = ['name']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'category',
+        'get_priority_display',
+        'due_date',
+        'completed',
+        'is_overdue'
+    ]
+    list_filter = ['completed', 'priority', 'category']
+    search_fields = ['title', 'description']
+    list_editable = ['completed']
+    list_per_page = 20
+
+    actions = ['mark_completed', 'mark_incomplete']
+
+    def mark_completed(self, request, queryset):
+        queryset.update(completed=True)
+    mark_completed.short_description = "Mark selected tasks as completed"
+
+    def mark_incomplete(self, request, queryset):
+        queryset.update(completed=False)
+    mark_incomplete.short_description = "Mark selected tasks as incomplete"
+
+
--- todo_app/models.py
+++ todo_app/models.py
@@ -11,6 +11,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 class Task(models.Model):
     PRIORITY_CHOICES = [
         ('H', 'High'),
